21b33da26d4c557d1c75a5da32c46a920df0a5ae
Slesha Mishra <sleshamishra4@gmail.com>
2019-05-05 10:21:50 +1000

    StudentFrontend/Login-Basic

--- uts-helps-system.spa/src/App.js
+++ uts-helps-system.spa/src/App.js
@@ -5,21 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <from>
+          <div>
+            <label>User Name</label>
+            <input type="text"></input>
+            </div>  
+
+            <div>
+              <label>Password</label>
+              <input type="password"></input>
+            </div>
+            <div>
+              <input type="submit" value="Login"></input>
+            </div>
+        </from>        
+        
       </div>
     );
   }
