998fa72197632e841ca68ce164ca403e9676f03f
Yu Hin Tsui <12708184@student.uts.edu.au>
2019-05-17 15:13:38 +1000

    Set up student frontend

--- uts-helps-system.spa/src/components/App.js
+++ uts-helps-system.spa/src/components/App.js
@@ -5,18 +5,21 @@ import Login from "./Login";
 import RegistrationPersonalDetails from "./RegistrationPersonalDetails";
 import Dashboard from "./Dashborad";
 import Admin from "./Admin";
+import {Header, Footer} from '../common'
 
 
 export default class App extends Component {
     render(){
         return(
             <div>
+                <Header/>
                 <BrowserRouter>
                 <Route exact path = "/" component = {Login}/>
                 <Route exact path = "/RegistrationPersonalDetails" component = {RegistrationPersonalDetails}/>
                 <Route exact path = "/Dashboard" component = {Dashboard}/>
                 <Route exact path = "/Admin" component = {Admin}/>
                 </BrowserRouter>
+                <Footer/>
             </div>
             
 
