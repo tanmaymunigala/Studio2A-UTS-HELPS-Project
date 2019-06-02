4c34dfbed211355ecaab677cd55c97eba1efa1d5
Slesha Mishra <sleshamishra4@gmail.com>
2019-06-02 16:13:13 +1000

    routing for admin

--- uts-helps-system.spa/src/routes/login/AdminSide/WorkshopAdmin.js
+++ uts-helps-system.spa/src/routes/login/AdminSide/WorkshopAdmin.js
@@ -1,12 +1,11 @@
-
 import React, {Component} from "react";
 //import {Link} from "react-router-dom";
 
-export default class Admin extends Component {
+export default class WorkshopAdmin extends Component {
     render(){
         return(
             <div>
-                <label>Admin Login Goes Here</label>
+                <label>Admin Workshops Goes Here</label>
             </div>
         )
     }
