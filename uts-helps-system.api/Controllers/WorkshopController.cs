e3d9449c5674da184f0fe6d84377b9621e5fad84
tanmaymunigala <tanmay.munigala@gmail.com>
2019-06-04 00:29:12 +1000

    Cleaned up Booking and Workshop controller code by 1. Adding validations 2. Refactoring 3. Removing empty lines 4. Made dates internationally compliable 5. Enabled and implemented logging

--- uts-helps-system.api/Controllers/WorkshopController.cs
+++ uts-helps-system.api/Controllers/WorkshopController.cs
@@ -7,6 +7,7 @@ using uts_helps_system.api.Models;
 using uts_helps_system.api.Data;
 using uts_helps_system.api.Security;
 using uts_helps_system.api.ResourceManagement;
+using Microsoft.Extensions.Logging;
 
 namespace uts_helps_system.api.Controllers
 {
@@ -14,55 +15,94 @@ namespace uts_helps_system.api.Controllers
     [ApiController]
     public class WorkshopController : ControllerBase
     {
+        private readonly ILogger _logger;
         private readonly ApplicationDbContext _context;
         private readonly TokenManager _tokenManager;
-        public WorkshopController(ApplicationDbContext context){
+        public WorkshopController(ILoggerFactory loggerFactory, ApplicationDbContext context)
+        {
+            _logger = loggerFactory.CreateLogger<WorkshopController>();
             _context = context;
-
         }
-    
 
-    [Route("CreateWorkshop/{WorkshopName}/{WorkshopDesc}/{WorkshopDateTime}")]
-    public async Task<Boolean> CreateWorkshop(int workshopId, string workshopName, string workshopDesc, DateTime workshopDateTime){
-        
-        var WorkshopModel = new Workshop(){
-            WorkshopName = workshopName,
-            WorkshopDesc = workshopDesc,
-            WorkshopDateTime = workshopDateTime,
+        [Route("CreateWorkshop/{WorkshopName}/{WorkshopDesc}/{WorkshopDateTime}")]
+        public async Task<Boolean> CreateWorkshop(int workshopId, string workshopName, string workshopDesc, DateTime workshopDateTime)
+        {
+            var workshopModel = new Workshop()
+            {
+                WorkshopName = workshopName,
+                WorkshopDesc = workshopDesc,
+                WorkshopDateTime = workshopDateTime,
+            };
+            _context.WorkshopValues.Add(workshopModel);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation($"Workshop added is {workshopModel}");
+            return true;
+        }
 
-        };
-        _context.WorkshopValues.Add(WorkshopModel);
-        await _context.SaveChangesAsync();
-        
-        return true;
-    }
         [Route("ListWorkshops")]
-    public List<Workshop> ListWorkshops(){
-
-       return _context.WorkshopValues.ToList();
-
-    }
-
+        public List<Workshop> ListWorkshops()
+        {
+            if (_context.WorkshopValues.Count() > 0)
+            {
+                var workshopList = _context.WorkshopValues.ToList();
+                _logger.LogInformation($"Workshop found is {workshopList}");
+                return workshopList;
+            }
+            else
+            {
+                _logger.LogInformation("There are no workshops available currently");
+                return null;
+            }
+        }
 
         [Route("DeleteWorkshop/{workShopId}")]
-    public async Task<Boolean> DeleteWorkshop(int workshopId){
-        var FoundWorkshop = _context.WorkshopValues.Find(workshopId);
-        _context.WorkshopValues.Remove(FoundWorkshop);
-        await _context.SaveChangesAsync();
+        public async Task<Boolean> DeleteWorkshop(int workshopId)
+        {
+            if (_context.WorkshopValues.Count() > 0)
+            {
+                var foundWorkshop = _context.WorkshopValues.Find(workshopId);
+                if (foundWorkshop != null)
+                {
+                    _context.WorkshopValues.Remove(foundWorkshop);
+                    await _context.SaveChangesAsync();
+                    _logger.LogInformation($"Workshop deleted is {foundWorkshop}");
+                    return true;
+                }
+                else
+                {
+                    _logger.LogInformation("No workshop found with given ID");
+                    return false;
+                }
+            }
+            else
+            {
+                _logger.LogInformation("There are no workshops available currently");
+                return false;
+            }
+        }
 
-       return true;
-
+        [Route("GetWorkshop/{workShopId}")]
+        public Workshop GetWorkshop(int workshopId)
+        {
+            if (_context.WorkshopValues.Count() > 0)
+            {
+                var foundWorkshop = _context.WorkshopValues.Find(workshopId);
+                if (foundWorkshop != null)
+                {
+                    _logger.LogInformation($"Workshop found is {foundWorkshop}");
+                    return foundWorkshop;
+                }
+                else
+                {
+                    _logger.LogInformation("No workshop found with given ID");
+                    return null;
+                }
+            }
+            else
+            {
+                _logger.LogInformation("There are no workshops currently");
+                return null;
+            }
+        }
     }
-
-            [Route("GetWorkshop/{workShopId}")]
-    public Workshop GetWorkshop(int workshopId){
-        var FoundWorkshop = _context.WorkshopValues.Find(workshopId);
-       return FoundWorkshop;
-
-    }
-
-
-
-    
-}
 }
